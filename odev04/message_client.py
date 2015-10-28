37149a0ac0b4b081825dd548ab8602c768688eb1
fulyaturan <fulyaturan0@gmail.com>
2015-10-28 16:17:29 +0200

    Update message_client.py

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -3,14 +3,36 @@ __author__ = 'fturan'
 import socket
 import threading
 
+class readThread (threading.Thread):
+    def __init__(self, sa):
+        threading.Thread.__init__(self)
+        self.sa = sa
+    def run(self):
+        while True:
+            message = self.sa.recv(1024)
+            print message
+        self.sa.close()
+    print 'bbb'
+
+
+class writeThread (threading.Thread):
+    def __init__(self, sa):
+        threading.Thread.__init__(self)
+        self.sa = sa
+    def run(self):
+        while True:
+            konsol=raw_input()
+            self.sa.send(konsol)
+        self.sa.close()
+
+
 s = socket.socket()
-host =socket.gethostname()
+host = socket.gethostname()
 port = 12345
 s.connect((host, port))
 
-while True:
-    s.send('heeey')
-    message=s.recv(1024)
-    print message
 
-s.close()
+rThread = readThread(s)
+rThread.start()
+wThread = writeThread(s)
+wThread.start()
