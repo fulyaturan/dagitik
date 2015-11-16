5ba75fcfb6bf64cb2f8b941b89a483b9c66d7c8f
fulyaturan <fulyaturan0@gmail.com>
2015-11-16 21:53:48 +0200

    Update and rename message_server to message_server.py

--- odev05/message_server.py
+++ odev05/message_server.py
@@ -5,6 +5,67 @@ import Queue
 import time
 fihrist={}
 
+class LoggerThread (threading.Thread):
+    def __init__(self, name, logQueue, fid):
+            threading.Thread.__init__(self)
+            self.name = name
+            self.lQueue = logQueue
+# dosyayi appendable olarak ac
+            self.fid =open("log.txt","a")
+
+    def log(self,message):
+# gelen mesaji zamanla beraber bastir
+        t = time.ctime()
+        self.fid.write(t + message )
+        self.fid.flush()
+    def run(self):
+        self.log("Starting " + self.name)
+
+        while True:
+
+            # lQueue'da yeni mesaj varsa
+            # self.log() methodunu cagir
+            if not lQueue.empty():
+                to_be_logged = self.lQueue.get()
+                self.log(to_be_logged)
+
+        self.log("Exiting" + self.name)
+        self.fid.close()
+        
+        
+class WriteThread(threading.Thread):
+    def  __init__(self, name, cSocket, address, threadQueue, logQueue ):
+         threading.Thread.__init__(self)
+         self.name = name
+         self.cSocket = cSocket
+         self.address = address
+         self.lQueue = logQueue
+         self.tQueue = threadQueue
+
+    def run(self):
+        self.lQueue.put("Starting" + self.name)
+        while True:
+
+
+            if self.threadQueue.qsize() > 0:
+                queue_message = self.threadQueue.get()
+
+                if queue_message[0:3]=="MSG":
+
+                    message_to_send = "MSG " + queue_message[4:]
+
+
+                elif queue_message[0:3]=="SAY":
+                    message_to_send = "SAY " + queue_message[4:]
+
+                else:
+                    message_to_send = "SYS "
+
+            self.cSocket.send(message_to_send)
+
+        self.lQueue.put("Exiting"  + self.name)
+        
+
 class ReadThread (threading.Thread):
 
     def __init__(self,name,cSocket, address, threadQueue):
@@ -62,10 +123,18 @@ class ReadThread (threading.Thread):
             #self.csend(response)
         elif data[0:3] == "SAY":
             message = data[4:]
+            print message
+            for nick in fihrist:
+                if nick != self.nickname:
+                    self.threadQueue.put(('SAY' +message))
+                    
+            self.cSocket.send("SOK")        
+            
         elif data[0:3] == "MSG":
             if not to_nickname in self.fihrist.keys():
                 response = "MNO"
             else :
+                
                 queue_message = (to_nickname, self.nickname, message)
                 # gonderilecek threadQueueyu fihristten alip icine yaz
                 self.fihrist[to_nickname].put(queue_message)
