3977cfba1a7071f34993b6005e8f987e8b2d4e45
fulyaturan <fulyaturan0@gmail.com>
2015-12-29 15:00:03 +0200

    Update negotiator.py

--- proje/negotiator.py
+++ proje/negotiator.py
@@ -6,6 +6,24 @@ import time
 
 connect_point_list={}
 connect_point={}
+class ServerThread(threading.Thread):
+    def  __init__(self):
+         threading.Thread.__init__(self)
+         self.cSocket = socket.socket()
+         self.ip = '127.0.0.1'
+         self.port=12345
+         self.cSocket.bind((self.ip,self.port))
+         self.cSocket.listen(4)
+
+    def run(self):
+        print 'burasi benim baglanti bekledigim yer'
+        while True:
+
+            c,addr=self.cSocket.accept()
+            readThread=ServerReadThread("serverreadthread",c,addr)
+            print 'Got connection from', addr
+            readThread.start()
+            
 class ServerReadThread(threading.Thread):
     def  __init__(self, name, cSocket, address):
          threading.Thread.__init__(self)
@@ -21,13 +39,15 @@ class ServerReadThread(threading.Thread):
         data=data.strip()
 
         if data[0:5]=='HELLO':
-
+            print "parserda HELLO if'ine girdi."
             response='SALUT' + str('N')
             self.cSocket.send(response)
 
         if data[0:5]=='CLOSE':
             self.cSocket.send('BUBYE')
             self.cSocket.close()
+            if self.ip in connect_point_list.keys():
+                del connect_point_list[self.ip]
 
         if data[0:5]=='REGME':
             data1=data[6:].split(":")
@@ -36,7 +56,7 @@ class ServerReadThread(threading.Thread):
             port=data1[1]
             self.ip=ip
             self.port=port
-            connect_point={ip:port}
+            connect_point={'ip':'port'}
 
             if connect_point_list.has_key(ip,port):
                 response='REGOK'+str(currentTime)
@@ -44,7 +64,7 @@ class ServerReadThread(threading.Thread):
 
             else :
                 response='REGWA'
-                connect_point_list[(connect_point)]='W',str(currentTime)
+                connect_point_list[(connect_point)]=('W',str(currentTime))
                 print connect_point_list
                 self.cSocket.send(response)
 
@@ -65,15 +85,15 @@ class ServerReadThread(threading.Thread):
                   
         self.cSocket.close()
 
+class ClientReadThread(threading.Thread)
 
 class ClientThread(threading.Thread):
-    def  __init__(self, name, cSocket, address, threadQueue ):
+    def  __init__(self, name, cSocket, address ):
          threading.Thread.__init__(self)
          self.name = name
          self.cSocket = cSocket
          self.address = address
 
-         self.tQueue = threadQueue
 
     def cparser(self,data):
          data=data.strip()
@@ -81,14 +101,8 @@ class ClientThread(threading.Thread):
         
 
     def run(self):
-        port = 12345
-        s.connect(('', port))
 
-
-port=12345
-threadQueue=Queue.Queue()
-s = socket.socket()
-st = ServerThread("ServerThread", s,port ,threadQueue)
+st = ServerThread()
 st.start()
-ct = ClientThread("ClientThread", s, threadQueue)
-ct.start()
+#ct = ClientThread("ClientThread", s, threadQueue)
+#ct.start()
